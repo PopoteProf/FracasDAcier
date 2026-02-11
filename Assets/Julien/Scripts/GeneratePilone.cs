d1167d35ae94f8543b08a922c15b0209037de8d6
Julien Durand <Juliend1305@gmail.com>
2026-02-11 12:13:20 +0100

    Add pilone

--- Assets/Julien/Scripts/GeneratePilone.cs
+++ Assets/Julien/Scripts/GeneratePilone.cs
@@ -14,10 +14,11 @@ namespace Julien.Scripts
         [SerializeField] private List<Vector2> _uvs = new List<Vector2>();
         [SerializeField] private List<Vector4> _tangents = new List<Vector4>();
         [SerializeField] private List<int> _triangles = new List<int>();
-        
+
         [Header(" parameters")] 
-        public int Hauteur;
-        public int Largeur;
+        public int Offset;
+        public float Hauteur;
+        public float Largeur;
 
         private void Start()
         {
@@ -42,12 +43,18 @@ namespace Julien.Scripts
             Vector3 g = new Vector3(1, 0, 1); // 6 
             Vector3 h = new Vector3(1, 1, 1); // 7
             
-            AddQuad(a,b,d,c);
-            AddQuad(c,d,h,g);
-            AddQuad(f,b,a,e);
-            AddQuad(h,f,e,g);
+            // AddQuad(a,b,d,c);
+            // AddQuad(c,d,h,g);
+            // AddQuad(f,b,a,e);
+            // AddQuad(h,f,e,g);
             
-            GenerateSquare(a,b,c,d,e,f,g,h);
+            //GenerateSquare(a,b,c,d,e,f,g,h);
+            for (int i = 0; i < Hauteur; i++)
+            {
+                GenerateSegment(new Vector3(0,Vector3.zero.y + i * Offset,0));
+            }
+            
+            
             
             mesh.vertices = _vertices.ToArray();
             mesh.triangles = _triangles.ToArray();
@@ -75,6 +82,38 @@ namespace Julien.Scripts
             _triangles.Add(index + 2);
             _triangles.Add(index + 3);
         }
+
+        public void RecusiveSquar(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, Vector3 v5, Vector3 v6, Vector3 v7, Vector3 v8)
+        {
+            for (int i = 0; i < Hauteur; i++)
+            {
+                GenerateSquare(v1, v2, v3, v4, v5, v6, v7, v8);
+
+                v1 = v2;
+                v3 = v4;
+                v7 = v8;
+                v5 = v6;
+
+                v2 = new Vector3(v2.x, v2.y * i, v2.z);
+                v4 = new Vector3(v4.x, v4.y * i, v4.z);
+                v6 = new Vector3(v6.x, v6.y * i, v6.z);
+                v8 = new Vector3(v8.x, v8.y * i, v8.z);
+            }
+        }
+
+        public void GenerateSegment(Vector3 origine)
+        {
+            Vector3 a = new Vector3(origine.x - Largeur / 2,origine.y + 0, origine.z - Largeur / 2); // 0
+            Vector3 b = new Vector3(origine.x - Largeur / 2,origine.y + 1, origine.z - Largeur / 2); // 1
+            Vector3 c = new Vector3(origine.x + Largeur / 2,origine.y + 0, origine.z - Largeur / 2); // 2
+            Vector3 d = new Vector3(origine.x + Largeur / 2,origine.y + 1, origine.z - Largeur / 2); // 3 
+            Vector3 e = new Vector3(origine.x - Largeur / 2,origine.y + 0, origine.z + Largeur / 2); // 4
+            Vector3 f = new Vector3(origine.x - Largeur / 2,origine.y + 1, origine.z + Largeur / 2); // 5
+            Vector3 g = new Vector3(origine.x + Largeur / 2,origine.y + 0, origine.z + Largeur / 2); // 6 
+            Vector3 h = new Vector3(origine.x + Largeur / 2,origine.y + 1, origine.z + Largeur / 2); // 7
+            
+            GenerateSquare(a,b,c,d,e,f,g,h);
+        }
         
         public void GenerateSquare(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, Vector3 v5, Vector3 v6, Vector3 v7, Vector3 v8)
         {
@@ -92,34 +131,35 @@ namespace Julien.Scripts
             _triangles.Add(index);
             _triangles.Add(index + 1);
             _triangles.Add(index + 2);
-        
-            _triangles.Add(index);
+            
+            _triangles.Add(index + 2);
+            _triangles.Add(index + 1);
+            _triangles.Add(index + 3);
+            
             _triangles.Add(index + 2);
             _triangles.Add(index + 3);
+            _triangles.Add(index + 6);
             
-            _triangles.Add(index);
+            _triangles.Add(index + 6);
             _triangles.Add(index + 3);
-            _triangles.Add(index + 4);
-        
-            _triangles.Add(index);
-            _triangles.Add(index + 4);
-            _triangles.Add(index + 5);
-            
-            _triangles.Add(index);
-            _triangles.Add(index + 5);
-            _triangles.Add(index + 6);
-        
-            _triangles.Add(index);
-            _triangles.Add(index + 6);
             _triangles.Add(index + 7);
             
-            _triangles.Add(index);
+            _triangles.Add(index + 6);
             _triangles.Add(index + 7);
-            _triangles.Add(index + 8);
-        
-            _triangles.Add(index);
-            _triangles.Add(index + 8);
-            _triangles.Add(index + 9);
+            _triangles.Add(index + 4);
+            
+            _triangles.Add(index + 4);
+            _triangles.Add(index + 7);
+            _triangles.Add(index + 5);
+            
+            _triangles.Add(index + 4);
+            _triangles.Add(index + 5);
+            _triangles.Add(index + 0);
+            
+            _triangles.Add(index + 0);
+            _triangles.Add(index + 5);
+            _triangles.Add(index + 1);
+            
         }
     }
 }
